a8d178d14ed1a17e8ad4ca71ca8d634e86f2fc5d
Hillary Koros <koroshillary12@gmail.com>
2024-12-19 12:55:31 +0300

    Updated the db to be spatially enabled

--- Impact/models.py
+++ Impact/models.py
@@ -10,6 +10,5 @@ class Impacts(models.Model):
     def __str__(self):
         return self.name
     
-
-class Meta:
-    verbose_name_plural = 'Impacts'
+    class Meta:
+        verbose_name_plural = "Impacts"
--- flood_watch_system/settings.py
+++ flood_watch_system/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal309.dll'
+GEOS_LIBRARY_PATH = r'C:\OSGeo4W\bin\geos_c.dll'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'flood_watch_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'flood_watch_system', 
+        'USER': 'postgres',
+        'PASSWORD': 'Icpac2025',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

b409c02cdb6da2da06b8af809a779e431b51093d
Hillary Koros <koroshillary12@gmail.com>
2024-12-19 12:30:34 +0300

    updated the models and migrated into the db

--- Impact/admin.py
+++ Impact/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Impacts 
 # Register your models here.
+
+#create a class called ImpactsAdmin that inherits from admin.ModelAdmin and has the following fields:
+
+class ImpactsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description' , 'location')
+
+
+admin.site.register(Impacts, ImpactsAdmin)
+
--- Impact/models.py
+++ Impact/models.py
@@ -6,5 +6,10 @@ from django.db import models
 class Impacts(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    location = models.CharField(max_length=100)
     def __str__(self):
         return self.name
+    
+
+class Meta:
+    verbose_name_plural = 'Impacts'

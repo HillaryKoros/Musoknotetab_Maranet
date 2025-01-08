6eec8fba016aca94da1b5f1b11d5cd5ac66b6ac4
Hillary Koros <hkoros@icpac.net>
2025-01-09 02:29:25 +0300

    Revert "updated new models for timeseries and sector data"
    This reverts commit bfe398e634022c7f0f10f7bafae24f46316e2c50.

---
+++ backend/Impact/admin.py
@@ -0,0 +1,57 @@
+from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
+from .models import (
+    AffectedGrazingLand, AffectedPopulation, ImpactedGDP, AffectedCrops,
+    AffectedLivestock, AffectedRoads, DisplacedPopulation
+)
+
+class BaseImpactAdmin(LeafletGeoAdmin):
+    list_display = (
+        'gid_0',
+        'name_0', 
+        'name_1',
+        'engtype_1',
+        'lack_cc',
+        'cod',
+        'stock',
+        'flood_tot',
+        'flood_perc'
+    )
+    search_fields = ('name_0', 'name_1', 'cod')
+    list_filter = ('name_0', 'engtype_1')
+    
+    # # Leaflet settings
+    # settings_overrides = {
+    #     'DEFAULT_CENTER': (0.0, 36.0),  # Centered on East Africa
+    #     'DEFAULT_ZOOM': 4,
+    #     'MIN_ZOOM': 3,
+    #     'MAX_ZOOM': 18,
+    # }
+
+@admin.register(AffectedPopulation)
+class AffectedPopulationAdmin(BaseImpactAdmin):
+    pass
+
+@admin.register(ImpactedGDP)
+class ImpactedGDPAdmin(BaseImpactAdmin):
+    pass
+
+@admin.register(AffectedCrops)
+class AffectedCropsAdmin(BaseImpactAdmin):
+    pass
+
+@admin.register(AffectedGrazingLand)
+class AffectedGrazingLandAdmin(BaseImpactAdmin):
+    pass
+
+@admin.register(AffectedLivestock)
+class AffectedLivestockAdmin(BaseImpactAdmin):
+    pass
+
+@admin.register(AffectedRoads)
+class AffectedRoadsAdmin(BaseImpactAdmin):
+    pass
+
+@admin.register(DisplacedPopulation)
+class DisplacedPopulationAdmin(BaseImpactAdmin):
+    pass
---
+++ backend/Impact/views.py
@@ -0,0 +1,54 @@
+from drf_spectacular.utils import extend_schema
+from drf_spectacular.openapi import AutoSchema
+from rest_framework import viewsets
+from .serializers import (
+    AffectedPopulationSerializer, ImpactedGDPSerializer, AffectedCropsSerializer,
+    AffectedRoadsSerializer, DisplacedPopulationSerializer, AffectedLivestockSerializer,
+    AffectedGrazingLandSerializer
+)
+from .models import (
+    AffectedPopulation, ImpactedGDP, AffectedCrops, AffectedGrazingLand,
+    AffectedLivestock, AffectedRoads, DisplacedPopulation
+)
+
+@extend_schema(tags=['affected-population'])
+class AffectedPopulationViewSet(viewsets.ModelViewSet):
+    schema = AutoSchema()
+    queryset = AffectedPopulation.objects.all()
+    serializer_class = AffectedPopulationSerializer
+
+@extend_schema(tags=['impacted-gdp'])
+class ImpactedGDPViewSet(viewsets.ModelViewSet):
+    schema = AutoSchema()
+    queryset = ImpactedGDP.objects.all()
+    serializer_class = ImpactedGDPSerializer
+
+@extend_schema(tags=['affected-crops'])
+class AffectedCropsViewSet(viewsets.ModelViewSet):
+    schema = AutoSchema()
+    queryset = AffectedCrops.objects.all()
+    serializer_class = AffectedCropsSerializer
+
+@extend_schema(tags=['affected-roads'])
+class AffectedRoadsViewSet(viewsets.ModelViewSet):
+    schema = AutoSchema()
+    queryset = AffectedRoads.objects.all()
+    serializer_class = AffectedRoadsSerializer
+
+@extend_schema(tags=['displaced-population'])
+class DisplacedPopulationViewSet(viewsets.ModelViewSet):
+    schema = AutoSchema()
+    queryset = DisplacedPopulation.objects.all()
+    serializer_class = DisplacedPopulationSerializer
+
+@extend_schema(tags=['affected-livestock'])
+class AffectedLivestockViewSet(viewsets.ModelViewSet):
+    schema = AutoSchema()
+    queryset = AffectedLivestock.objects.all()
+    serializer_class = AffectedLivestockSerializer
+
+@extend_schema(tags=['affected-grazing-land'])
+class AffectedGrazingLandViewSet(viewsets.ModelViewSet):
+    schema = AutoSchema()
+    queryset = AffectedGrazingLand.objects.all()
+    serializer_class = AffectedGrazingLandSerializer
